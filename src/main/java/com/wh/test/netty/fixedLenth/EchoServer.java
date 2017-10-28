83fc0dba4a150e9e063cecaa6df7d677564af29a
wanghui <wanghui@auto-fis.com>
2017-10-28 15:33:23 +0800

    消息固定长度解码器

---
+++ src/main/java/com/wh/test/netty/fixedLenth/EchoServer.java
@@ -0,0 +1,74 @@
+package com.wh.test.netty.fixedLenth;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created by wanghui on 17-10-28.
+ */
+public class EchoServer {
+
+    public void bind(int port) throws Exception{
+
+        //配置服务端的nio线程
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup wordGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, wordGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1000)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+
+                            //每个消息固定20的长度
+                            ch.pipeline().addLast(new FixedLengthFrameDecoder(20));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+
+            //绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        }finally {
+            //优雅的退出，释放线程资源
+            bossGroup.shutdownGracefully();
+            wordGroup.shutdownGracefully();
+        }
+
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int port = 8081;
+
+        if(args != null && args.length > 0){
+            try {
+                port = Integer.parseInt(args[0]);
+            }catch (NumberFormatException e){
+                //采用默认值
+            }
+        }
+
+        new EchoServer().bind(port);
+
+    }
+
+}
